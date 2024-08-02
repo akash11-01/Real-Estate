0b35d4900ed082ba195664ca7a3e8a53d77ce7f6
Akash Jaiswar <akashjaiswar221@gmai.com>
2024-08-02 16:32:33 +0530

    Add error handling middleware to backend

--- backend/index.js
+++ backend/index.js
@@ -6,16 +6,28 @@ import authRouter from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
 
-app.use(express.json());
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
+
+  
 app.use("/api/user", userRouter);
 app.use('/api/auth',authRouter);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.use((err, req, res, next) => {
+    const statuscode = err.statuscode || 500;
+    const massage = err.massage || "Internal server error";
+
+    return res.status(statuscode).json({
+        success: false,
+        statuscode,
+        massage
+    })
+})
