41cb92482669cbc04f857913abc1d0bf4c163137
Akash Jaiswar <akashjaiswar221@gmai.com>
2024-08-07 14:23:04 +0530

    complete update user functionality

--- real-state/src/pages/Profile.jsx
+++ real-state/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { app } from '../firebase';
 import {
   getDownloadURL,
@@ -7,14 +7,20 @@ import {
   ref,
   uploadBytesResumable,
 } from 'firebase/storage';
+import { updateUserFailure, updateUserStart, updateUserSuccess } from '../redux/user/userSlice';
 
 export default function Profile() {
-  const { currentUser, error } = useSelector((state) => state.user);
+  const { currentUser, error,loading } = useSelector((state) => state.user);
   const [file, setFile] = useState(undefined)
   const fileRef = useRef(null);
   const [filePerc, setFilePerc] = useState(0);
   const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+
   const [fileUploadError, setFileUploadError] = useState(false);
+  const dispatch = useDispatch();
+  console.log(formData);
+  
 
   useEffect(() => {
     if (file) {
@@ -27,7 +33,6 @@ export default function Profile() {
     const fileName = new Date().getTime() + file.name;
     const storageRef = ref(storage, fileName);
     const uploadTask = uploadBytesResumable(storageRef, file);
-
     uploadTask.on(
       'state_changed',
       (snapshot) => {
@@ -44,10 +49,40 @@ export default function Profile() {
       }
     );
   }
+
+  const handleChange = async (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value })
+  }
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body:JSON.stringify(formData)
+        })
+        const data = await res.json();
+        if(data.success === false){
+          dispatch(updateUserFailure(data.message));
+          return;
+        }
+        dispatch(updateUserSuccess(data));
+        setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error.message))
+    }
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
-      <form className='flex flex-col gap-4'>
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
 
         <input
           onChange={(e) => setFile(e.target.files[[0]])}
@@ -55,11 +90,11 @@ export default function Profile() {
           ref={fileRef}
           hidden
           accept='image/' />
-        <img onClick={() => fileRef.current.click()} 
-          src={formData.avatar || currentUser.avatar} 
+        <img onClick={() => fileRef.current.click()}
+          src={formData.avatar || currentUser.avatar}
           alt="profile"
           className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' />
-       
+
         <p className='text-sm self-center'>
           {
             fileUploadError ? (<span className='text-red-700 font-semibold'>Error Image Upload (image must be less than 2 mb)</span>)
@@ -68,16 +103,27 @@ export default function Profile() {
                   : ('')
           }
         </p>
-        <input type="text" placeholder='username' id='username'
-          className='border p-3 rounded-lg' />
 
-        <input type="email" placeholder='email' id='email'
-          className='border p-3 rounded-lg' />
+        <input type="text"
+          placeholder='username'
+          defaultValue={currentUser.username}
+          id='username'
+          className='border p-3 rounded-lg'
+          onChange={handleChange} />
+
+        <input
+          type="email"
+          placeholder='email'
+          defaultValue={currentUser.email}
+          id='email'
+          className='border p-3 rounded-lg'
+          onChange={handleChange} />
 
         <input type="password" placeholder='password' id='password'
-          className='border p-3 rounded-lg' />
+          className='border p-3 rounded-lg'
+          onChange={handleChange} />
 
-        <button className='bg-slate-700 text-white rounded-lg
+        <button  className='bg-slate-700 text-white rounded-lg
          p-3 hover:opacity-95 disabled:opacity-80'>
           UPDATE
         </button>
@@ -87,6 +133,13 @@ export default function Profile() {
         <span className='text-red-700 cursor-pointer'>Delete account</span>
         <span className='text-red-700 cursor-pointer'>Sign out</span>
       </div>
+
+      <p className='text-red-700'>
+        {error ? error : ''}
+      </p>
+      <p className='text-green-700 mt-5'>
+        {updateSuccess ? 'user updated successfully' : ''}
+      </p>
     </div>
   )
 }
