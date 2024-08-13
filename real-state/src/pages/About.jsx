52ab9358278bdc1644783ea5311751da6e25acac
Akash Jaiswar <akashjaiswar221@gmai.com>
2024-08-13 18:32:07 +0530

    project completed

--- real-state/src/pages/About.jsx
+++ real-state/src/pages/About.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <div className="px-4 py-20 max-w-6xl mx-auto">
+        <h1 className="text-3xl font-bold mb-4 text-slate-800">About Real Estate</h1>
+        <p className="text-slate-700 mb-4">
+          Real Estate is a leading real estate agency that specializes in
+          helping clients buy, sell, and rent properties in the most desirable
+          neighborhoods. Our team of experienced agents is dedicated to
+          providing exceptional service and making the buying and selling
+          process as smooth as possible.
+        </p>
+        <p className="text-slate-700 mb-4">
+          Real Estate is a leading real estate agency that specializes in
+          helping clients buy, sell, and rent properties in the most desirable
+          neighborhoods. Our team of experienced agents is dedicated to
+          providing exceptional service and making the buying and selling
+          process as smooth as possible.
+        </p>
+        <p className="text-slate-700 mb-4">
+          Real Estate is a leading real estate agency that specializes in
+          helping clients buy, sell, and rent properties in the most desirable
+          neighborhoods. Our team of experienced agents is dedicated to
+          providing exceptional service and making the buying and selling
+          process as smooth as possible.
+        </p>
+      </div>
+    </div>
+  );
 }
