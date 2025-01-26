daa3f04d28a326c39eb47eafd3a48f300cfefea6
Akash Jaiswar <akashjaiswar221@gmai.com>
2025-01-26 23:38:15 +0530

    signin functionalty bug fixed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -50,7 +50,7 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, "Invalid creadentials"));
     }
 
-    const { password: pass, ...rest } = validUser._;
+    const { password: pass, ...rest } = validUser._doc;
 
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     res
